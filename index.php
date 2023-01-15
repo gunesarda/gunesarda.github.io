24fee31a4d8abf8f3a6b954d90ab4775a264e419
gunesarda <101694412+gunesarda@users.noreply.github.com>
2023-01-15 14:07:20 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/home.css">
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark" id="navUst" style="background-color: #987554;">
+        <a class="navbar-brand" href="#">
+            <img src="img/kemence.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            KEMENCEM.ORG
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarScroll">
+            <ul class="navbar-nav mr-auto my-2 my-lg-0 navbar-nav-scroll" style="max-height: 100px;">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Anasayfa <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Dersler</a>
+                </li>
+                <li class="nav-item dropdown active">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+                        Kemençeyi Anlamak
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control mr-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+    <!-- <div id="kutu1">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <img src="img/kemenceArkaplan.png" alt="image" class="img-fluid">
+                </div>
+                <div class="col-md-6">
+                    <h2>Başlık</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nisi ut aliquet aliquet, massa leo tristique magna, a tempus nisl dui vel augue.</p>
+                </div>
+            </div>
+        </div>
+    </div> -->
+
+
+    <!-- <div class="container" id="kutu1">
+        <table>
+            <tr class="grid-column-count:2 display-grid">
+                <td class="display-grid"><img src="img/kemenceArkaplan.png" class="img-fluid"></td>
+                <td class="grid-column-count 2">Karadeniz kemençesi, Türkiye’nin Karadeniz bölgesinde çalınan üç telli bir çalgıdır. Keman gibi bir yayı vardır. Türkiye haricinde Gürcistan ve Yunanistan’da (mübadele sırasında göç edenler) da Karadeniz kemençesi çalınmaktadır. Bölgelere göre kemençe farklılık gösterebilir. Trabzon, Rize bölgelerinde daha çok kalın sesli kemençeler çalınırken, Giresun ve Ordu bölgelerinde ise daha çok ince sesli kemençeler çalınmaktadır. Yunanistan’da bulunan kemençeler ise “Rum Kemençesi” olarak bilinir, sesi Trabzon yöresine ait kemençelere benzer. Biraz daha tok ve kalın sesi vardır. </td>
+            </tr>
+        </table>
+    </div> -->
+
+    <div class="container" id="kutu1">
+        <div class="row">
+            <div class="col-sm">
+                <img src="img/kemenceArkaplan.png" class="img-fluid">
+            </div>
+            <div class="col-sm" id="kutuIciYazi">
+                Karadeniz kemençesi, Türkiye’nin Karadeniz bölgesinde çalınan üç telli bir çalgıdır. Keman gibi bir yayı vardır. Türkiye haricinde Gürcistan ve Yunanistan’da (mübadele sırasında göç edenler) da Karadeniz kemençesi çalınmaktadır. Bölgelere göre kemençe farklılık gösterebilir. Trabzon, Rize bölgelerinde daha çok kalın sesli kemençeler çalınırken, Giresun ve Ordu bölgelerinde ise daha çok ince sesli kemençeler çalınmaktadır. Yunanistan’da bulunan kemençeler ise “Rum Kemençesi” olarak bilinir, sesi Trabzon yöresine ait kemençelere benzer. Biraz daha tok ve kalın sesi vardır.
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container" id="kutu1">
+        <div class="row">
+            <div class="col-sm">
+                <img src="img/kemenceArkaplan.png" class="img-fluid">
+            </div>
+            <div class="col-sm" id="kutuIciYazi">
+                Karadeniz kemençesi, Türkiye’nin Karadeniz bölgesinde çalınan üç telli bir çalgıdır. Keman gibi bir yayı vardır. Türkiye haricinde Gürcistan ve Yunanistan’da (mübadele sırasında göç edenler) da Karadeniz kemençesi çalınmaktadır. Bölgelere göre kemençe farklılık gösterebilir. Trabzon, Rize bölgelerinde daha çok kalın sesli kemençeler çalınırken, Giresun ve Ordu bölgelerinde ise daha çok ince sesli kemençeler çalınmaktadır. Yunanistan’da bulunan kemençeler ise “Rum Kemençesi” olarak bilinir, sesi Trabzon yöresine ait kemençelere benzer. Biraz daha tok ve kalın sesi vardır.
+            </div>
+        </div>
+    </div>
+
+    
+    <!-- <div class="container" id="kutu1">
+
+        <div class="container" id="inlineKutu1">
+            <img src="img/kemenceArkaplan.png" alt="">
+        </div>
+
+
+        <div class="container" id="inlineKutu2">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius in tempora, soluta expedita animi autem vero debitis voluptatibus nulla similique!</p>
+        </div>
+    
+    </div> -->
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+</body>
+
+</html>
